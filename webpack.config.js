16c13e03d7cda7aee8fa4a24cf6cb726e50fb6bc
Ronna Mae Firmo <ronnwiseley@gmail.com>
2018-03-22 20:02:38 +0800

    fix: fix webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -14,22 +14,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.js?/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
-      }, 
-      {
-        test: /\.css$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
-        ]
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: 'public/',
+              outputPath: 'files/'
+            }  
+          }
+        ]
       }
     ]
   },
