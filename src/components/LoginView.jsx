9cd1a7adb421697be745977811935054a67509cb
Ronna Mae Firmo <ronnwiseley@gmail.com>
2018-03-23 18:53:48 +0800

    refactor: change login background pic

--- src/components/LoginView.jsx
+++ src/components/LoginView.jsx
@@ -5,7 +5,7 @@ export default class LoginView extends React.Component {
   render() {
     return (
       <div>
-        <Image fluid src='/images/blurred.jpg' /> 
+        <Image fluid src='/images/howto.gif' /> 
         <Modal open basic size='mini'>
           <Segment vertical
             style={{ paddingBottom: '6em' }}>
