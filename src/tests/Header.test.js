9507ce8dda1f928567f4b04596c2613c6ee47309
Ronna Mae Firmo <ronnwiseley@gmail.com>
2018-03-24 15:18:01 +0800

    test: add test for Header

---
+++ src/tests/Header.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { shallow } from './setup'
+import Header from '../components/Header'
+import { Menu, Container, Icon, Button } from 'semantic-ui-react'
+
+let wrapper
+
+describe('<Header />', () => {
+  beforeEach(() => {
+    wrapper = shallow(<Header />)
+  })
+
+  it('renders itself', () => {
+    expect(wrapper).toHaveLength(1)
+  })
+
+  it('contains <Menu>', () => {
+    expect(wrapper.find(Menu)).toHaveLength(1)
+  })
+
+  it('contains <Container>', () => {
+    expect(wrapper.find(Container)).toHaveLength(1)
+  })
+
+  it('contains 5 <Menu.Item>', () => {
+    expect(wrapper.find(Menu.Item)).toHaveLength(5)
+  })
+
+  it('contains 2 <Icon>', () => {
+    expect(wrapper.find(Icon)).toHaveLength(2)
+  })
+
+  it('contains <Menu.Menu>', () => {
+    expect(wrapper.find(Menu.Menu)).toHaveLength(1)
+  })
+
+  it('contains <Button>', () => {
+    expect(wrapper.find(Button)).toHaveLength(1)
+  })
+
+  it('contains 2 <Button.Content>', () => {
+    expect(wrapper.find(Button.Content)).toHaveLength(2)
+  })
+})
