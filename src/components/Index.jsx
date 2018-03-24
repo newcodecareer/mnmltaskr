74156ddfd4588a18bfd86b4bdd1e44f7119df366
Ronna Mae Firmo <ronnwiseley@gmail.com>
2018-03-24 18:53:05 +0800

    feat: add LoginView route to Index

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import 'semantic-ui-css/semantic.min.css'
+import { Route } from 'react-router-dom'
+
+import LoginView from './LoginView'
 import ItemsContainer from '../containers/ItemsContainer'
 import Body from './Body'
-import LoginView from './LoginView'
 
 export default class Index extends React.Component {
   render() {
     return (
       <div>
+        <Route path='/login' component={LoginView} />
         <ItemsContainer />
         <Body />
       </div>
